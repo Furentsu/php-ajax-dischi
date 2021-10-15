fec45a8d3d0a76e9eedeb2613d822cced12c146c
Furentsu <eugeniofranzoso@gmail.com>
2021-10-15 18:10:10 +0200

    Added main section

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
+<!DOCTYPE html>
+
 <?php 
     
 include_once __DIR__ . '/db/database.php'; 
 
 ?>
 
-<?php foreach ($dischiArray as $dischi); { ?>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP Dischi</title>
+</head>
+<body>
 
-    <?php foreach($dischi as $singoloDisco) { ?>
-        <p> <?php echo $singoloDisco['title'] ?> </p>
-    <?php } ?>
+    <!-- Main -->
+    <section class="main_color_bg">
+        <div class="container">
+
+            <?php foreach ($dischiArray as $dischi) { ?>
+                <?php foreach ($dischi as $singoloDisco) { ?>
+                    <div class="album">
+                        <div class="img-container">
+                            <img src="<?php echo $singoloDisco['poster'] ?>" alt="<?php echo $singoloDisco['title'] ?>">
+                        </div>
+
+                        <h3> <?php echo $singoloDisco['title'] ?> </h3>
+                        <h3> <?php echo $singoloDisco['author'] ?> </h3>
+                        <h4> <?php echo $singoloDisco['year'] ?> </h4>
+                    </div>
+                <?php } ?>
+            <?php } ?>
+
+        </div>
+    </section>
     
-<?php } ?>
+</body>
+</html>
 
